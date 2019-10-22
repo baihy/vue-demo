691e8b55e58553744a7638815d1f4fc210820e93
huayang.bai <1053091115@qq.com>
2019-10-22 11:34:00 +0800

    初始化导入

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
+    <div v-if="!repoName">loding...</div>
+    <div v-else>最流程的框架是：{{repoName}}</div>
   </div>
 </template>
 
 <script>
+
     export default {
-        name: 'App'
+        name: 'App',
+        data: function () {
+            return {
+                repoName: ''
+            }
+        },
+        mounted: function () {
+            const url = 'https://api.github.com/search/repositories?q=vue&sort=stars'
+            this.$http.get(url).then(function (response) {
+                console.info(response)
+                this.repoName = response.data.items[0].name
+            }, function () {
+                console.info('请求失败！！！')
+            })
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 // 注意：可以在main中，引入任何文件， 且这些文件在所有的模块中，都是有效的
+
+Vue.use(VueResource) // 内部会给VM对象和组件添加一个属性：$http
 
 new Vue({
   el: '#app',
